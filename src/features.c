51e92c49e0121cac557f65411edcea155fdbbac7
victor-ricard <victor.ricard@etu.estia.fr>
2025-05-26 09:50:53 +0200

    ECRITURE DIMENSION & suppression estia-images

--- src/features.c
+++ src/features.c
@@ -1,8 +1,8 @@
-#include <estia-image.h>
 #include <stdio.h>
 
 #include "features.h"
 #include "utils.h"
+#include <stdlib.h>
 
 /**
  * @brief Here, you have to code features of the project.
@@ -15,6 +15,19 @@ void helloWorld() {
     printf("Hello World !");
 
 }
-void dimension (char *source_path){
+
+int dimension(const char *source_path, int *WIDTH, int *HEIGHT) {
+    unsigned char *data = NULL;
+    int channel_count;
     
+
+    if (!read_image_data(source_path, &data, WIDTH, HEIGHT, &channel_count)) {
+        fprintf(stderr, "Erreur de lecture de l'image : %s\n", source_path);
+        return 0;
+    }
+
+    printf("Dimensions de l'image : %d, %d\n", *WIDTH, *HEIGHT);
+
+    free(data);
+    return 1;
 }
